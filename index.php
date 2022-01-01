0beaac62a8a2a4db6f79dc1fbe698202883a956a
chrram <chr.ram.94@gmail.com>
2022-01-02 00:14:29 +0100

    Views of the register and login are done.

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
+    session_start();
 
-    echo "Welcome to taskmaker!";
+    if(!empty($_POST["register_password"]) && !empty($_POST["register_email"])){
+
+        $email = $_POST["register_email"];
+        $pwd = $_POST["register_password"];
+        
+        header("Location: views/login.php?action=success");
+        exit;
+    }
+
+    if(empty($_SESSION['loggedin'])) {
+        header("Location: views/login.php");
+    }
 
 ?>
